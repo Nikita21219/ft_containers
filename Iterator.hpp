7736a6d3823406ba1d68fd0734ddfe779c712983
Beldecar Clarinda <bclarind@at-j5.msk.21-school.ru>
2022-09-19 19:59:47 +0300

    before iterator_traits

--- Iterator.hpp
+++ Iterator.hpp
@@ -16,17 +16,21 @@ namespace ft {
 
     template <typename T>
     struct iterator_traits<T *> {
-        typedef random_access_iterator_tag  iterator_category;
-        typedef T                           value_type;
-        typedef ptrdiff_t                   distance_type;
-        typedef T*                          pointer;
-        typedef T&                          reference;
+        typedef random_access_iterator_tag iterator_category;
+        typedef T                          value_type;
+        typedef ptrdiff_t                  distance_type;
+        typedef T*                         pointer;
+        typedef T&                         reference;
     };
 
     template< class T >
     struct iterator_traits<const T*> {
-
-    }; //TODO fill this struct
+        typedef random_access_iterator_tag iterator_category;
+        typedef T                          value_type;
+        typedef ptrdiff_t                  difference_type;
+        typedef const T*                   pointer;
+        typedef const T&                   reference;
+    };
 
     template<
         bool isConst,
